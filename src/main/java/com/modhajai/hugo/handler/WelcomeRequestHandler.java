3466f4cd30b4aabc1dccd61776c663484f91ca34
Jai Modha <jaimodha@icloud.com>
2021-06-08 14:50:22 -0700

    Token check & OAuth library added

--- src/main/java/com/modhajai/hugo/handler/WelcomeRequestHandler.java
+++ src/main/java/com/modhajai/hugo/handler/WelcomeRequestHandler.java
@@ -3,6 +3,8 @@ package com.modhajai.hugo.handler;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Optional;
 
@@ -16,8 +18,24 @@ public class WelcomeRequestHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
-        return handlerInput.getResponseBuilder()
-                .withSpeech("Welcome from Hugo!")
-                .build();
+        String accessToken = handlerInput.getRequestEnvelope().getContext().getSystem().getUser().getAccessToken();
+
+        if (accessToken != null) {
+            // TODO Validate the token an retrieve user claims
+
+            DecodedJWT jwt = JWT.decode(accessToken);
+            return handlerInput.getResponseBuilder()
+                    .withSpeech("Welcome to Hugo -> " + jwt.getClaim("given_name") + "!")
+                    .build();
+        } else {
+            String speechText = "You must have a Humana account. "
+                    + "Please use the Alexa app to link your Amazon account "
+                    + "with your Humana Account.";
+            return handlerInput.getResponseBuilder()
+                    .withSpeech(speechText)
+                    .withLinkAccountCard()
+                    .build();
+        }
     }
 }
+
